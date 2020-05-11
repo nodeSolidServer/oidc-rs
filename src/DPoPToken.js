3103fd1ba0447ff3ce71916bc3a415946b28fd94
jaxoncreed <jaxoncreed@gmail.com>
2020-05-11 16:02:06 -0400

    Validates jwk thumbprint in DPoP token

--- src/DPoPToken.js
+++ src/DPoPToken.js
@@ -1,5 +1,7 @@
 const AccessToken = require('./AccessToken')
 const JWT = require('jsonwebtoken')
+const { jwkThumbprintByEncoding } = require('jwk-thumbprint')
+const {UnauthorizedError} = require('./errors/index')
 const jwkToPem = require('jwk-to-pem')
 
 class DPoPToken extends AccessToken {
@@ -17,26 +19,46 @@ class DPoPToken extends AccessToken {
   }
 
   async validatePoPToken () {
-
+    // decode header token
+    const decodedHeaderToken = await JWT.decode(this.dpopHeader, { json: true, complete: true })
+    // verify the dpop token is signed by the valid JWK
     try {
-      // decode header token
-      const decodedHeaderToken = await JWT.decode(this.dpopHeader, { json: true, complete: true })
-      // verify the auth token contains the correct public key
-      await JWT.verify(this.dpopHeader, jwkToPem(this.jwt.payload.cnf))
-      // verify htu and htm
-      let requiredHtu = new URL(`${this.serverUri}${this.req.path}`)
-      if (this.isSubdomain(requiredHtu.host, this.req.get('host'))) {
-        requiredHtu.host = this.req.get('host')
-      }
-      requiredHtu = requiredHtu.toString()
-      if (decodedHeaderToken.payload.htu !== requiredHtu) {
-        throw new Error(`htu ${decodedHeaderToken.payload.htu} does not match ${requiredHtu}`)
-      }
-      if (decodedHeaderToken.payload.htm !== this.req.method) {
-        throw new Error(`htm ${decodedHeaderToken.payload.htm} does not match ${this.req.method}`)
+      await JWT.verify(this.dpopHeader, jwkToPem(decodedHeaderToken.header.jwk))
+    } catch (err) {
+      throw new UnauthorizedError({
+        error: 'invalid_token',
+        error_description: err.message
+      })
+    }
+    // verify the the dpop header JWK is equal to the access token cnf
+    try {
+      const dpopHeaderThumbprint = jwkThumbprintByEncoding(decodedHeaderToken.header.jwk, "SHA-256", 'base64url')
+      if (this.jwt.payload.cnf.jkt !== dpopHeaderThumbprint) {
+        throw new Error('Access token cnf does not match the DPoP header JWK')
       }
     } catch (err) {
-      return false;
+      throw new UnauthorizedError({
+        error: 'invalid_token',
+        error_description: err.message
+      })
+    }
+    // verify htu and htm
+    let requiredHtu = new URL(`${this.serverUri}${this.req.path}`)
+    if (this.isSubdomain(requiredHtu.host, this.req.get('host'))) {
+      requiredHtu.host = this.req.get('host')
+    }
+    requiredHtu = requiredHtu.toString()
+    if (decodedHeaderToken.payload.htu !== requiredHtu) {
+      throw new UnauthorizedError({
+        error: 'invalid_token',
+        error_description: `htu ${decodedHeaderToken.payload.htu} does not match ${requiredHtu}`
+      })
+    }
+    if (decodedHeaderToken.payload.htm !== this.req.method) {
+      throw new Error({
+        error: 'invalid_token',
+        error_description: `htm ${decodedHeaderToken.payload.htm} does not match ${this.req.method}`
+      })
     }
   }
 
