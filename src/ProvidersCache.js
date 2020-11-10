98bd8fcba298637b813830c71cf0bab196abf5e4
jaxoncreed <jaxoncreed@gmail.com>
2020-11-10 13:55:18 -0500

    Update src/ProvidersCache.js
    Co-authored-by: Ruben Verborgh <ruben@verborgh.org>

--- src/ProvidersCache.js
+++ src/ProvidersCache.js
@@ -58,7 +58,7 @@ class ProvidersCache {
    * @returns {Promise}
    */
   discover (issuer) {
-    return fetch(`${issuer}${issuer.endsWith('/') ? '' : '/'}.well-known/openid-configuration`)
+    return fetch(issuer.replace(/\/*$/, '/.well-known/openid-configuration')
       //.then(validateStatus)
       .then(response => response.json())
   }
